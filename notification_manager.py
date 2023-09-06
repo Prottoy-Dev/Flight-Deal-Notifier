77956a456951af244c2b8be58a2c00fddd12bd8b
Prottoy-Dev <133051274+Prottoy-Dev@users.noreply.github.com>
2023-09-06 07:29:28 +0600

    Update notification_manager.py

--- notification_manager.py
+++ notification_manager.py
@@ -1,18 +1,30 @@
-from twilio.rest import Client
-import os
-from dotenv import load_dotenv
+# Import required libraries and modules
+from twilio.rest import Client  # Twilio library for sending SMS messages
+import os  # Operating System library for working with environment variables
+from dotenv import load_dotenv  # Library for loading environment variables
+import smtplib  # SMTP library for sending email messages
+import requests  # Library for making HTTP requests
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Retrieve environment variables for API URLs, phone numbers, and credentials
+users_url = os.getenv("users_url")
 sender_number = os.getenv("sender_number")
 receiver_number = os.getenv("receiver_number")
 twilio_account_SID = os.getenv("twilio_SID")
 twilio_auth_Token = os.getenv("twilio_Token")
+my_email = os.getenv("my_email")
+my_pass = os.getenv("my_pass")
 
+# Fetch user data from an external source (assumed to be a URL)
+data = requests.get(url=users_url).json()["users"]
 
+# Define the NotificationManager class responsible for sending notifications
 class NotificationManager:
     """
-    The NotificationManager class is responsible for sending notifications with flight deal details using the Twilio API.
+    The NotificationManager class is responsible for sending notifications with flight deal
+    details using the Twilio API.
 
     Attributes:
     - client (Client): An instance of the Twilio Client class for sending SMS messages.
@@ -29,6 +41,12 @@ class NotificationManager:
 
         Returns:
         - Message: An instance of the Twilio Message class representing the sent message.
+
+    - send_mail(self, body):
+        Sends an email message with the specified body to multiple recipients.
+
+        Parameters:
+        - body (str): The text content of the email message.
 
     Example Usage:
     notification_manager = NotificationManager()
@@ -57,3 +75,23 @@ class NotificationManager:
             from_=sender_number,
             to=receiver_number
         )
+
+    def send_mail(self, body):
+        """
+        Sends an email message with the specified body to multiple recipients.
+
+        Parameters:
+        - body (str): The text content of the email message.
+        """
+        data = requests.get(url=users_url).json()["users"]
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email, my_pass)
+            for x in data:
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs=x["email"],
+                    msg=f"Subject: New Low Price Flight \n\n {body}",
+                )
+
+
